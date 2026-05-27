bfc7ebf37541e55361c7bc36421baa33d07027e7
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2026-05-27 13:37:27 +0300

    feat: update utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,19 +1,20 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import quotes from "$lib/data/quotes.json";
 
 export function random(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 export function shuffleArr(arr: any[]) {
-  for (let i = arr.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]];
-  }
+	for (let i = arr.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * (i + 1));
+		[arr[i], arr[j]] = [arr[j], arr[i]];
+	}
 
-  return arr;
+	return arr;
 }
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+const quotesShuffled: typeof quotes = shuffleArr(quotes);
+
+export function getRandomQuote() {
+	return quotesShuffled[Math.floor(Math.random() * quotesShuffled.length)];
 }
